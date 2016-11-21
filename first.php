6ca91eaab2274e22b4f5cf3045c2afc7f442f67e
helio <heliossamu@gmail.com>
2016-11-21 13:46:19 -0200

    facebook user name

--- first.php
+++ first.php
@@ -47,9 +47,6 @@
 				'default_graph_version' => 'v2.2',
 			]);
 
-			$user = $fb->getUser();
-			echo $user;
-
 			$helper = $fb->getRedirectLoginHelper();
 			try {
 			  $accessToken = $helper->getAccessToken();
@@ -67,7 +64,19 @@
 			  	// Logged in!
 			  	$_SESSION['facebook_access_token'] = (string) $accessToken;
 
+			  	try {
+					// Returns a `Facebook\FacebookResponse` object
+					$response = $fb->get('/me?fields=id,name', $accessToken);
+				} catch(Facebook\Exceptions\FacebookResponseException $e) {
+				    echo 'Graph returned an error: ' . $e->getMessage();
+				    exit;
+				} catch(Facebook\Exceptions\FacebookSDKException $e) {
+				    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+				    exit;
+				}
 
+				$user = $response->getGraphUser();
+				echo 'Name: ' . $user['name'];
 			}
 
 			/*
