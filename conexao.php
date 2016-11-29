99c08724ac52ba376ea3ce5e4679beef37b7dc2d
helio <heliossamu@gmail.com>
2016-11-29 16:04:03 -0200

    conexao

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 
 <?php
-	$con_string = "host=ec2-54-243-203-104.compute-1.amazonaws.com port=5432 dbname=dddllblhhc280js user=nlxboferoxcyrf password=9UxFDo4CgJaq19oGkuC9WWznD3";
+	$con_string = "host=ec2-54-243-203-104.compute-1.amazonaws.com port=5432 dbname=dddllblhhc280j user=nlxboferoxcyrf password=9UxFDo4CgJaq19oGkuC9WWznD3 sslmode=require";
 
 	$conexao = pg_connect($con_string) or die ("Não foi possivel conectar ao servidor PostGreSQL");
 	
