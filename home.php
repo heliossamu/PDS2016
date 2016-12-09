8ec2706fd7d8546609f1c32e37e359e85270af10
helio <heliossamu@gmail.com>
2016-12-09 15:24:43 -0200

    fr

--- home.php
+++ home.php
@@ -47,8 +47,8 @@
 
 			try {
 				// Returns a `Facebook\FacebookResponse` object
-				
-				$response = $fb->get('/me/friends&fields=id,name', $accessToken);
+
+				$response = $fb->get('/me/friends?fields=id,name', $accessToken);
 				$graphObject = $response->getGraphObject()->asArray();	
 			} catch(Facebook\Exceptions\FacebookResponseException $e) {
 			    echo 'Graph returned an error: ' . $e->getMessage();
